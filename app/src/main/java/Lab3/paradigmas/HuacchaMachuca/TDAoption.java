0e9dd0d773eb1efcdef17492a81b4b48e9694449
DemisHuaccha <128428584+DemisHuaccha@users.noreply.github.com>
2023-11-28 22:26:21 -0300

    Add files via upload
    se agrega projecto lab3 de paradigmas, con tda option casi completo

---
+++ app/src/main/java/Lab3/paradigmas/HuacchaMachuca/TDAoption.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab3.paradigmas.HuacchaMachuca;
+
+/**
+ *
+ * @author Demis
+ */
+public class TDAoption {
+    int code;
+    String message;
+    int chatbotcodelink;
+    int initialflowcodelink;
+    
+    public int getcode(){
+        return code;
+    }
+    public String getmessage(){
+        return message;
+    }
+    public int getchatbotcodelink(){
+        return chatbotcodelink;
+    }
+    public int getflowcodelink(){
+        return initialflowcodelink;
+    }
+    
+    
+}
